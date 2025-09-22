0b57fc3217de0f27c87e0db035f82663fbfaa4c1
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-09-22 14:09:59 +0200

    Modify Player component Add state to edit name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,24 @@
-export default Player = ({ name, symbol }) => {
+import { useState } from "react";
+
+const Player = ({ name, symbol }) => {
+  const [isEditting, setIsEditting] = useState(false);
+
+  const handleEditClick = () => setIsEditting((prev) => !prev);
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditting ? (
+          <input type="text" required />
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
+
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditting ? "Save" : "Edit"}</button>
     </li>
   );
 };
+
+export default Player;
